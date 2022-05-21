03adb65bc05d6940e63dd96636aecdf7eb90cb68
Ingo Ruhnke <grumbel@gmail.com>
2022-05-21 13:33:17 +0200

    Add Nix flake build files

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Uinput helper library for C++";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    tinycmmc.url = "gitlab:grumbel/cmake-modules";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+    tinycmmc.inputs.flake-utils.follows = "flake-utils";
+
+    strutcpp.url = "gitlab:grumbel/strutcpp";
+    strutcpp.inputs.nixpkgs.follows = "nixpkgs";
+    strutcpp.inputs.flake-utils.follows = "flake-utils";
+    strutcpp.inputs.tinycmmc.follows = "tinycmmc";
+
+    logmich.url = "gitlab:logmich/logmich";
+    logmich.inputs.nixpkgs.follows = "nixpkgs";
+    logmich.inputs.flake-utils.follows = "flake-utils";
+    logmich.inputs.tinycmmc.follows = "tinycmmc";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, tinycmmc, strutcpp, logmich }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          uinputcpp = pkgs.stdenv.mkDerivation {
+            pname = "uinputcpp";
+            version = "0.1.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            cmakeFlags = [ "-DBUILD_TESTS=ON" ];
+            doCheck = true;
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.pkgconfig
+            ];
+            buildInputs = [
+              tinycmmc.defaultPackage.${system}
+              logmich.defaultPackage.${system}
+              strutcpp.defaultPackage.${system}
+
+              pkgs.util-linux
+              pkgs.gtest
+              pkgs.fmt
+              pkgs.glib
+              pkgs.dbus-glib
+              pkgs.pcre
+            ];
+           };
+        };
+        defaultPackage = packages.uinputcpp;
+      });
+}
